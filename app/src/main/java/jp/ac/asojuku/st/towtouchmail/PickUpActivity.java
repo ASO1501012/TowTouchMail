e228521e96845d1160eb11e8b0fa61cc9851b84f
高松　昂大 <Garudo24@takamatsukoudai-no-MacBook-Air.local>
2016-09-23 12:19:18 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/asojuku/st/towtouchmail/PickUpActivity.java
@@ -0,0 +1,54 @@
+package jp.ac.asojuku.st.towtouchmail;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.Random;
+
+public class PickUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_up);
+        Button btnSend = (Button) this.findViewById(R.id.button);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rgPlace = (RadioGroup) findViewById(R.id.rg_place);
+                int checkedId = rgPlace.getCheckedRadioButtonId();
+                String strPlace = ((RadioButton) findViewById(checkedId)).getText().toString();
+                EditText edit01 = (EditText) findViewById(R.id.editText);
+                String title = edit01.getText().toString();
+                Random r = new Random();
+                int n = r.nextInt(3);
+
+                Uri uri;
+
+                if(n == 1){
+                    Resources res = getResources();
+                    uri = Uri.parse("mailto:" + res.getString(R.string.mail_to1).toString());
+                }else if(n == 2){
+                    Resources res = getResources();
+                    uri = Uri.parse("mailto:" + res.getString(R.string.mail_to2).toString());
+                }else{
+                    Resources res = getResources();
+                    uri = Uri.parse("mailto:" + res.getString(R.string.mail_to3).toString());
+                }
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                intent.putExtra(Intent.EXTRA_SUBJECT, title);
+                intent.putExtra(Intent.EXTRA_TEXT, strPlace + "に迎えに来て");
+                startActivity(intent);
+            }
+        });
+    }
+}
